1a6447618f1bcd68087537739f47987f5f2a1fb6
Syntactic Sugar <timothyakho@gmail.com>
2016-11-28 15:58:59 -0500

    Rewrote history

--- ProbabilisticGenerator.py
+++ ProbabilisticGenerator.py
@@ -18,24 +18,24 @@ weights if they are provided.
 """
 
 #/usr/bin/python
-# @copyright: (c)Copyright 2014, Intel Corporation All Rights Reserved.
+# @copyright: (c)Copyright 2014, THC All Rights Reserved.
 # The source code contained or described here in and all documents related
-# to the source code ("Material") are owned by Intel Corporation or its
-# suppliers or licensors. Title to the Material remains with Intel Corporation
+# to the source code ("Material") are owned by THC or its
+# suppliers or licensors. Title to the Material remains with THC
 # or its suppliers and licensors. The Material contains trade secrets and
-# proprietary and confidential information of Intel or its suppliers and
+# proprietary and confidential information of THC or its suppliers and
 # licensors.
 
 # The Material is protected by worldwide copyright and trade secret laws and
 # treaty provisions. No part of the Material may be used, copied, reproduced,
 # modified, published, uploaded, posted, transmitted, distributed, or disclosed
-# in any way without Intel's prior express written permission.
+# in any way without THC's prior express written permission.
 
 # No license under any patent, copyright, trade secret or other intellectual
 # property right is granted to or conferred upon you by disclosure or delivery
 # of the Materials, either expressly, by implication, inducement, estoppel or
 # otherwise. Any license under such intellectual property rights must be express
-# and approved by Intel in writing.
+# and approved by THC in writing.
 
 # @organization: NDG SDI Science
 # @summary: This file generates sentences from a PCFG in JSGF. Run it by entering 
